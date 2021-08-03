283b46ac2f520679c12db93098f694ef346493fd
DongGyun Han <me@donggyun.com>
2021-08-03 11:50:21 +0800

    add ThemeColor options

--- webhook.py
+++ webhook.py
@@ -1,11 +1,19 @@
 from datetime import datetime
+from enum import Enum
 import requests
+
+
+class ThemeColor(Enum):
+    INFO_COLOR = "5bc0de"
+    SUCCESS_COLOR = "5cb85c"
+    ERROR_COLOR = "d9534f"
+
 
 header = {"Content-Type": "application/json"}
 JSON_TEMPLATE = '''{
     "@type": "MessageCard",
     "@context": "http://schema.org/extensions",
-    "themeColor": "0076D7",
+    "themeColor": "%s",
     "summary": "%s",
     "sections": [{
         "activityTitle": "%s",
@@ -18,12 +26,13 @@ JSON_TEMPLATE = '''{
 '''
 
 
-def generate_payload(title: str, subtitle: str = "", facts: dict = {}):
+def generate_payload(title: str, subtitle: str, facts: dict, color: ThemeColor):
     facts["Updated"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     formatted_facts = [{'name': k, 'value': v} for k, v in facts.items()]
-    return JSON_TEMPLATE % (title, title, subtitle, str(formatted_facts))
+    return JSON_TEMPLATE % (color.value, title, title, subtitle, str(formatted_facts))
 
 
-def send_webhook(url: str, title: str, subtitle: str = "", facts: dict = {}):
-    content = generate_payload(title, subtitle, facts)
+def send_webhook(url: str, title: str, subtitle: str = "", facts: dict = {},
+                 color: ThemeColor = ThemeColor.INFO_COLOR):
+    content = generate_payload(title, subtitle, facts, color)
     requests.post(url, data=content, headers=header)
